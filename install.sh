1a7909e20cc36d83e88c3a9a568368d79dca84f1
Filipe Ligeiro Silva <filipe.ligeiro.silva@hotmail.com>
2020-11-14 12:22:46 +0000

    .

--- install.sh
+++ install.sh
@@ -2,54 +2,54 @@
 
 echo "[+] Checking for required dependencies..."
 if command -v git >/dev/null 2>&1 ; then
-  echo "[-] Git found!"
+	echo "[-] Git found!"
 else
-  echo "[-] Git not found! Aborting..."
-  echo "[-] Please install git and try again."
+	echo "[-] Git not found! Aborting..."
+	echo "[-] Please install git and try again."
 fi
 
 if [ -f ~/.gdbinit ] || [ -h ~/.gdbinit ]; then
-  echo "[+] backing up gdbinit file"
-  cp ~/.gdbinit ~/.gdbinit.back_up
+	echo "[+] backing up gdbinit file"
+	cp ~/.gdbinit ~/.gdbinit.back_up
 fi
 
 # download peda and decide whether to overwrite if exists
 if [ -d ~/peda ] || [ -h ~/.peda ]; then
-  echo "[-] PEDA found"
-  read -p "skip download to continue? (enter 'y' or 'n') " skip_peda
+	echo "[-] PEDA found"
+	read -p "skip download to continue? (enter 'y' or 'n') " skip_peda
 
-  if [ $skip_peda = 'n' ]; then
-    rm -rf ~/peda
-    git clone https://github.com/longld/peda.git ~/.gdb_plugins/peda
-  else
-    echo "PEDA skipped"
-  fi
+	if [ $skip_peda = 'n' ]; then
+		rm -rf ~/peda
+		git clone https://github.com/longld/peda.git ~/.gdb_plugins/peda
+	else
+		echo "PEDA skipped"
+	fi
 else
-  echo "[+] Downloading PEDA..."
-  git clone https://github.com/longld/peda.git ~/.gdb_plugins/peda
+	echo "[+] Downloading PEDA..."
+	git clone https://github.com/longld/peda.git ~/.gdb_plugins/peda
 fi
 
 
 # download pwndbg
 if [ -d ~/pwndbg ] || [ -h ~/.pwndbg ]; then
-  echo "[-] Pwndbg found"
-  read -p "skip download to continue? (enter 'y' or 'n') " skip_pwndbg
+	echo "[-] Pwndbg found"
+	read -p "skip download to continue? (enter 'y' or 'n') " skip_pwndbg
 
-  if [ $skip_pwndbg = 'n' ]; then
-    rm -rf ~/pwndbg
-    git clone https://github.com/pwndbg/pwndbg.git ~/.gdb_plugins/pwndbg
+	if [ $skip_pwndbg = 'n' ]; then
+		rm -rf ~/pwndbg
+		git clone https://github.com/pwndbg/pwndbg.git ~/.gdb_plugins/pwndbg
 
-    cd ~/.gdb_plugins/pwndbg
-    ./setup.sh
-  else
-    echo "Pwndbg skipped"
-  fi
+		cd ~/.gdb_plugins/pwndbg
+		./setup.sh
+	else
+		echo "Pwndbg skipped"
+	fi
 else
-  echo "[+] Downloading Pwndbg..."
-  git clone https://github.com/pwndbg/pwndbg.git ~/.gdb_plugins/pwndbg
+	echo "[+] Downloading Pwndbg..."
+	git clone https://github.com/pwndbg/pwndbg.git ~/.gdb_plugins/pwndbg
 
-  cd ~/.gdb_plugins/pwndbg
-  ./setup.sh
+	cd ~/.gdb_plugins/pwndbg
+	./setup.sh
 fi
 
 
@@ -63,21 +63,22 @@ cd ~/.gdb_peda_pwndbg_gef
 cp gdbinit ~/.gdbinit
 
 {
-  echo "[+] Creating files..."
-  sudo cp gdb-peda /usr/bin/gdb-peda &&\
-    sudo cp gdb-pwndbg /usr/bin/gdb-pwndbg &&\
-    sudo cp gdb-gef /usr/bin/gdb-gef
-  } || {
-    echo "[-] Permission denied"
-      exit
-    }
+	echo "[+] Creating files..."
+	cd ~/.gdb_peda_pwndbg_gef
+	sudo cp gdb-peda /usr/bin/gdb-peda &&\
+		sudo cp gdb-pwndbg /usr/bin/gdb-pwndbg &&\
+		sudo cp gdb-gef /usr/bin/gdb-gef
+	} || {
+		echo "[-] Permission denied"
+			exit
+		}
 
-  {
-    echo "[+] Setting permissions..."
-    sudo chmod +x /usr/bin/gdb-*
-  } || {
-    echo "[-] Permission denied"
-      exit
-    }
+	{
+		echo "[+] Setting permissions..."
+		sudo chmod +x /usr/bin/gdb-*
+	} || {
+		echo "[-] Permission denied"
+			exit
+		}
 
-  echo "[+] Done"
+	echo "[+] Done"
